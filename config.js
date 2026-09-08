b136807c885fde1faed94c0953b0390fe2c52fe7
jckchu147-li <jckchu147@gmail.com>
2026-09-08 23:23:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CASE_TRACKER_SETUP = {
-  GAS_API_URL: "YOUR_GAS_WEB_APP_URL",
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbwTVpSaEIrWMRDTXW4sxxyAqmB4yzoA62KglTgKAO_8DjuZV-ZFVgGtnaptj9hJJQ3j/exec",
   LIFF_ID: "2011510971-lvvgd8It"
 };
